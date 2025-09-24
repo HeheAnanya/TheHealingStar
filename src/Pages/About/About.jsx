d8449a673b76586375ace463c66900f645ea47eb
HeheAnanya <ananyanarang1411@gmail.com>
2025-09-24 11:40:47 +0530

    added footer to the about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Home/JSX/Navbar'
 import Hero from './JSX/Hero'
+import Footer from "../Home/JSX/Footer"
 
 const About = () => {
   return (
     <div>
         <Navbar/>
         <Hero/>
+        <Footer/>
     </div>
   )
 }
