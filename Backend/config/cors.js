fc6e1f3164223084779a6b97791e9ab1386806bd
HeheAnanya <ananyanarang1411@gmail.com>
2026-01-03 22:58:28 +0530

    Fix CORS for frontend

--- Backend/config/cors.js
+++ Backend/config/cors.js
@@ -1,6 +1,8 @@
 const cors = require("cors")
 const options = {
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173",
+        "https://thehealingstar.onrender.com"
+    ],
     methods:["GET","PUT","POST","DELETE"],
     allowedHeaders:["Content-Type", "Authorization"],
     credentials: true
